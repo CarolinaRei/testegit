7348ac7b01dc8860880a48cc35d350ed406511ce
José Costa <j.ricardo.n.costa@gmail.com>
2020-11-05 10:46:18 +0000

    Criada a acção B

--- testegit/Controllers/HomeController.cs
+++ testegit/Controllers/HomeController.cs
@@ -36,6 +36,11 @@ namespace testegit.Controllers
             return View();
         }
 
+        public IActionResult B()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {

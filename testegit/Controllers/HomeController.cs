4e5803a7adf57156e283983d3daa4c9345feb165
Frede404 <fredecabral@gmail.com>
2020-11-05 09:47:03 +0000

    Pagina Sobre

--- testegit/Controllers/HomeController.cs
+++ testegit/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace testegit.Controllers
             return View();
         }
 
+        public IActionResult Sobre()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
